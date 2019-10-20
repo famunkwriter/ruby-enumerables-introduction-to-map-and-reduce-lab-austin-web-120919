4cde86a4ed1fbdbcdda6ed94f8a7b7b47d98d520
Bretton B Holmes <famunkwriter@me.com>
2019-10-20 23:17:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,6 +60,8 @@ end
 def reduce_to_any_true(source_array)
   counter = 0
   while counter < source_array.size do
-    return true if source_array[counter]
+    return false if source_array[counter]
     counter += 1
   end
+  return true
+end
